4471d7111c7cabffafc9f548a505783110851e48
mikeangelsoldier <mikeangelsoldier1@gmail.com>
2019-05-04 22:40:51 -0500

    "nombre de metodo"

--- src/java/modelo/GestorPlatilloBD.java
+++ src/java/modelo/GestorPlatilloBD.java
@@ -239,7 +239,7 @@ public class GestorPlatilloBD {
         }
     }
     
-     public List<Object> getPlatillosPorPuntaje(int idPlatillo) {
+     public List<Object> getPlatillosPuntaje(int idPlatillo) {
         /*Devuelve una lista con todos los usuarios 
          leidos de la base de datos*/
 
